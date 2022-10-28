456ace3cd9aca113ddf0a1ab9793ec18d45a7499
parami <thepassenger816@gmail.com>
2022-10-28 20:47:22 +0630

    Fix product no show and remove scroll reveal js and index js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,20 @@
-/*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+        nav = document.getElementById(navId)
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             nav.classList.toggle('show')
         })
     }
 }
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle', 'nav-menu')
 
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show')
@@ -24,37 +24,22 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
+    sections.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-        }else{
+        } else {
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
         }
     })
 }
 window.addEventListener('scroll', scrollActive)
-
-/*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2000,
-    delay: 200,
-//     reset: true
-});
-
-sr.reveal('.about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
-
 
 // custum code to add item
 
@@ -63,7 +48,7 @@ var additm = document.querySelector('.products-container');
 var addabt = document.querySelector('#about1');
 console.log(additm);
 
-function createCard([img,alt,product,category,price]){
+function createCard([img, alt, product, category, price]) {
     let code = `
     <div class="product" id="card">
          <img src="${img}" alt="${alt}">
@@ -77,13 +62,13 @@ function createCard([img,alt,product,category,price]){
 
 }
 
-let item1 =["images/drugs/Antibiotic(Metronidazole suspension)].jpg", 'photo alt text','Cherry Cough','cough suppressant','$2500 kyat']
-let item2 =["images/drugs/Antibiotic_Cefixime(Taxim-O).jpg", 'photo alt text','Cherry Cough','cough suppressant','$2500 kyat']
-let item3 =["images/drugs/CNS_Pramipexole dihydrochloride(Pramipex).jpg", 'photo alt text','Cherry Cough','cough suppressant','$2500 kyat']
-let item4 =["images/drugs/Menopause.jpg", 'photo alt text','Cherry Cough','cough suppressant','$2500 kyat']
-let item5 =["images/drugs/Multivitamin(Neurobest).jpg", 'photo alt text','Cherry Cough','cough suppressant','$2500 kyat']
-let item6 =["images/drugs/Multivitamin(Neurobion Forte).jpg", 'photo alt text','Cherry Cough','cough suppressant','$2500 kyat']
-let item7 =["images/drugs/Albendazole 400 mg.jpg", 'photo alt text','Cherry Cough','cough suppressant','$2500 kyat']
+let item1 = ["images/drugs/Antibiotic(Metronidazole suspension)].jpg", 'photo alt text', 'Metronidazole suspension', 'Antibiotic', '$2500 kyat']
+let item2 = ["images/drugs/Antibiotic_Cefixime(Taxim-O).jpg", 'photo alt text', 'Cefixime', 'Antibiotic', '$2500 kyat']
+let item3 = ["images/drugs/CNS_Pramipexole dihydrochloride(Pramipex).jpg", 'photo alt text', 'Pramipex', 'Pramipexole dihydrochloride', '$2500 kyat']
+let item4 = ["images/drugs/Menopause.jpg", 'photo alt text', 'Menopause', 'Menopause', '$2500 kyat']
+let item5 = ["images/drugs/Multivitamin(Neurobest).jpg", 'photo alt text', 'Neurobest', 'Multivitamin', '$2500 kyat']
+let item6 = ["images/drugs/Multivitamin(Neurobion Forte).jpg", 'photo alt text', 'Neurobion Forte', 'Multivitamin', '$2500 kyat']
+let item7 = ["images/drugs/Albendazole 400 mg.jpg", 'photo alt text', 'Albendazole', 'Antiprotozoal', '$2500 kyat']
 
 createCard(item1);
 createCard(item2);
@@ -95,7 +80,7 @@ createCard(item7);
 
 
 
-function createAbt([img,alt,product,category,price]){
+function createAbt([img, alt, product, category, price]) {
     let code = `
     <div class="product" id="card">
          <img src="${img}" alt="${alt}">
@@ -109,9 +94,9 @@ function createAbt([img,alt,product,category,price]){
 
 }
 
-let abt1 =["images/drugs/Multivamin(meditone).jpg", 'photo alt text','Cherry Cough','cough suppressant','$2500 kyat']
-let abt2 =["images/drugs/Multivitamin(Neurobest).jpg", 'photo alt text','Cherry Cough','cough suppressant','$2500 kyat']
-let abt3 =["images/drugs/Multivitamin_Thiamine(Thyflox).jpg", 'photo alt text','Cherry Cough','cough suppressant','$2500 kyat']
+let abt1 = ["images/drugs/Multivamin(meditone).jpg", 'photo alt text', 'Cherry Cough', 'cough suppressant', '$2500 kyat']
+let abt2 = ["images/drugs/Multivitamin(Neurobest).jpg", 'photo alt text', 'Cherry Cough', 'cough suppressant', '$2500 kyat']
+let abt3 = ["images/drugs/Multivitamin_Thiamine(Thyflox).jpg", 'photo alt text', 'Cherry Cough', 'cough suppressant', '$2500 kyat']
 
 createAbt(abt1);
 createAbt(abt2);
